2d20a69aa16e221a0fa0ef503ac926fd41c55bdf
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-03-03 16:31:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  elsif array.length >= 1
+    new_array = []
+    array.each_with_index do |person, index|
+      index_plus_one = index + 1
+      new_array.push("#{index_plus_one}. #{person} ")
+    end
+      puts "The line is currently: #{new_array.join}"
+  end
+end
